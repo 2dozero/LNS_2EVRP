4d13dcc34523f0cb46fa20e1e12ed2dca6f23d72
2dozero <will981116@gmail.com>
2024-06-14 13:59:22 +0900

    update

--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,5 @@
+using Statistics
+
 include("population.jl")
 include("read_tevrp.jl")
 include("destroy.jl")
@@ -20,13 +22,14 @@ end
 function LNS(instance::TEVRP_Instance)
     i = 0
     best_dist = Inf
+    count = 0
     update_count = 0
 
     first_level_routes, second_level_routes = initial_solution(instance)
     @show second_level_routes
     initial = deepcopy(second_level_routes)
 
-    while update_count < 100
+    while count < 500
         customer_pool, second_level_routes = random_removal(instance, second_level_routes, 3)
         first_level_routes, s_p = greedy_insertion(instance, customer_pool, first_level_routes, second_level_routes)
         
@@ -49,15 +52,49 @@ function LNS(instance::TEVRP_Instance)
 
         if s_p_val < best_dist
             best_dist = s_p_val
+            update_count += 1
         end
-        update_count += 1
+        count += 1
         # @show second_level_routes
-        @show update_count, best_dist
+        @show count, best_dist
     end
-    @show second_level_routes
-    return best_dist
+    # @show update_count
+    # @show second_level_routes
+    return best_dist, update_count
 end
 
-instance = read_tevrp("benchmark_instances/E-n22-k4-s6-17.txt")
-best_route_distance = LNS(instance)
-println("Best route distance: ", best_route_distance)
+# instance = read_tevrp("benchmark_instances/2a_E-n22-k4-s6-17.txt") # 2a
+# instance = read_tevrp("benchmark_instances/2b_E-n51-k5-s2-17.txt") # 2b-1
+# instance = read_tevrp("benchmark_instances/2b_E-n51-k5-s2-4-17-46.txt") # 2b-2
+# instance = read_tevrp("benchmark_instances/2c_E-n51-k5-s2-17.txt") # 2c-1
+# instance = read_tevrp("benchmark_instances/2c_E-n51-k5-s2-4-17-46.txt") # 2c-2
+# instance = read_tevrp("benchmark_instances/4b_Instance50-49.txt") # 4b
+# instance = read_tevrp("benchmark_instances/5_100-5-1.txt") #5-1
+# instance = read_tevrp("benchmark_instances/5_100-10-1.txt") #5-2
+instance = read_tevrp("benchmark_instances/5_200-10-1.txt") #5-3
+
+
+times = Float64[]
+best_dists = Float64[]
+counts = Int64[]
+
+for _ in 1:100
+    run_time = @elapsed begin
+        best_dist, update_count = LNS(instance)
+        push!(best_dists, best_dist)
+        push!(counts, update_count)
+    end
+    push!(times, run_time)
+end
+
+average_time = mean(times)
+average_best_dist = mean(best_dists)
+average_update_count = mean(counts)
+
+println("Average time for each run: ", average_time, " seconds")
+println("Average update count for each run: ", average_update_count)
+println("Average best distance for each run: ", average_best_dist)
+
+
+# best_route_distance = LNS(instance)
+# println("Best route distance: ", best_route_distance)
